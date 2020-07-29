80b8b83a723bddc37f2107f7ece13c927cc253d3
SergeyGurkov <biggreen.rm@gmail.com>
2020-07-29 19:47:13 +0300

    Write simple test without logic in order to understand how it works

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SmokeTest(TestCase):
+    """тест на токсичность"""
+    
+    def test_bad_math(self):
+        """Тест: неправильные математические расчёты"""
+        self.assertEqual(1 + 1, 3)
