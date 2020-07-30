c0163e471bb864af9114275652924778aec93d6d
SergeyGurkov <biggreen.rm@gmail.com>
2020-07-30 13:06:58 +0300

    FT now can check is there an opportunity to add element into To-Do list

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,6 +14,7 @@ class NewVisitorTest(unittest.TestCase):
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Тест: можно начать список и получить его позже"""
+        
         # Эрнест где-то услыхал, что списки дел экономят психическую энергию
         # которой ему маловато в последнее время
         # Он решил оценить домашнюю страницу такого приложения
@@ -19,19 +22,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # Он обращает внимание, что заголовок и шапка страницы намекают на список дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ему предлагают ввести в список дело, которым нельзя пренебречь
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
         # В текстовом поле Эрнест пишет "Привести себя в порядок"
         # Ведь в последнее время он много пил и пренебрегал бритьём
-
+        inputbox.send_keys('Привести себя в порядок')
+        
         # Когда он нажимает enter - страница обновляется и теперь страница
         # содержит "1: Привести себя в порядок" в качестве элемента
-
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == "1: Привести себя в порядок" for row in rows)
+        )
         # Текстовое поле по-прежнему приглашает Эрнеста сделать запись 
         # Что ж, пора "Убрать весь хлам на кухне"
-
+        self.fail('Закончить тест!')
         # Страница снова обновляется и теперь показывает оба элемента списка
 
         # Эрнесту интересно, запомнит ли сайт еге список. Далее он видит, что 
