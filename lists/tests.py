e233e34322d700df9a287ca3ab4263fca28f35c4
SergeyGurkov <biggreen.rm@gmail.com>
2020-08-01 17:05:47 +0300

    Refactored FT, add CSRF to form of to-do list, made universal MT for post and get requests

--- lists/tests.py
+++ lists/tests.py
@@ -9,10 +9,17 @@ from lists.views import home_page
 
         
 class HomePageTest(TestCase):
-'''Тест домашней страницы'''
-    
+    """Тест: домашняя страница"""
+
     def test_home_page_returns_correct_html(self):
         '''Тест: домашняя страница возращает правильный html'''
         
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+        
+    def test_can_save_POST_request(self):
+        '''Тест: можно сохранить POST-запрос'''
+        # item_text - это данные формы на home page '/'
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 
 def home_page(request):
     """Домашняя страница"""
-    
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '')
+        })
