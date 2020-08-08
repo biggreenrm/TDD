f6191e4e83634c501e95ad5a6eaba3f46c153021
SergeyGurkov <biggreen.rm@gmail.com>
2020-08-08 20:00:12 +0300

    set FT to test transitional server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
@@ -12,7 +13,12 @@ class NewVisitorTest(StaticLiveServerTestCase):
     MAX_WAIT = 10
     
     def setUp(self):
+        """Установка"""
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+        
         
     def tearDown(self):
         self.browser.quit()
