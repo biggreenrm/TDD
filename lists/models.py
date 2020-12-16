b5263eb535f2817fed4a8a9507dc7bf0ccd00ebe
biggreenrm <biggreen.rm@gmail.com>
2020-12-16 16:14:32 +0300

    Change Item model's Meta to maintain the uniqueness of such pairs as 'list' and 'text'

--- lists/models.py
+++ lists/models.py
@@ -13,3 +13,12 @@ class Item(models.Model):
     """Модель задачи из списка"""
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        # Задачи сортируются по порядку добавления от раннего к позднему
+        ordering = ('id',)
+        # В одном списке не может быть одинаковых задач
+        # unique_together = ('list', 'text')
